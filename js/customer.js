a242684c417186e1dcf45b36eccd43b505cb5057
dbxnr <kevinmalachimulligan@gmail.com>
2017-10-30 18:58:24 +0100

    Orders display in a receipt box

--- js/customer.js
+++ js/customer.js
@@ -1,10 +1,22 @@
-function Customer(){
+function Customer() {
+    // Constructor for a customer
+    // Should be able to customise order more, e.g. omitting or ordering multiples
     this.order = new FoodOrder()
-    
-    
-    // Very rudimentary ordering system - not sure how to update the HTML elegantly...
-    burgerTown.netEarning += this.order.orderTotal;
+
+    displayOrder(this.order)
+
+}
+
+function displayOrder(order) {
+    // Function to output the order as HTML
+    burgerTown.netEarning += order.orderTotal;
     document.getElementById('earnings').innerHTML = burgerTown.netEarning
     
-    document.getElementById('debug').innerHTML = JSON.stringify(this.order)
+    receiptBox = document.getElementById('receiptBox')
+    receiptBox.innerHTML = order.main.NAME + ": " + order.main.PRICE + "<br>"
+    + order.side.NAME + ": " + order.side.PRICE + "<br>"
+    + order.sauce.NAME + ": " + order.sauce.PRICE + "<br>"
+    + order.drink.NAME + ": " + order.drink.PRICE + "<br>"
+    + "TOTAL: " + order.orderTotal + "<br>"
+    + "<br><br>"
 }
