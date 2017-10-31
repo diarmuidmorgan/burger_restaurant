1770a7ecd588d2bb24f24c3346dd7134606def14
dbxnr <kevinmalachimulligan@gmail.com>
2017-10-31 16:27:32 +0100

    Customers have variable appetities

--- js/customer.js
+++ js/customer.js
@@ -1,9 +1,18 @@
 function Customer() {
     // Constructor for a customer
     // Should be able to customise order more, e.g. omitting or ordering multiples
+
+    // Returns a random number biased towards min
+    function biasedRandom(min, max) {
+        return Math.round(max / (Math.random() * max + min));
+    }
+
     this.anger = 60
-    this.hunger = 4
+    this.hunger = weightedRandom(20,100) + 1
     this.order = new FoodOrder(this.hunger)
     renderOrder(this.order)
+
+
+
 
 }
