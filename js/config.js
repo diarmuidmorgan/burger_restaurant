8b9980b16f0b9e1ae7f557c16b7ea0650c0d44f2
dbxnr <kevinmalachimulligan@gmail.com>
2017-11-14 13:28:20 +0000

    Tweak ordering system - no more empty orders

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 // Simulation variables
 
 const GAME_SPEED = 1000 // Speed in milliseconds
-const CUSTOMER_PROB = 0.5 // Probability for customer to arrive
+const CUSTOMER_PROB = 1 // Probability for customer to arrive
 
 const DEBUG_MODE = 1    // Toggle diagnostics
--- js/customer.js
+++ js/customer.js
@@ -8,7 +8,7 @@ function Customer() {
     }
 
     this.anger = 60
-    this.hunger = biasedRandom(20,100) + 1
+    this.hunger = biasedRandom(20,90) + 1
     this.order = new FoodOrder(this.hunger)
 
 
