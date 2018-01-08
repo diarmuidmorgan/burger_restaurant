1c21077d6effcc13866ade5520bada5e316efb76
dbxnr <kevinmalachimulligan@gmail.com>
2018-01-08 20:25:27 +0000

    queue fix

--- js/gameloop.js
+++ js/gameloop.js
@@ -1,22 +1,24 @@
 function gameLoop() {
 	// Runs through the customers and checks their status
-	for (var customer in burgerTown.queue) {
+	if (burgerTown.queue.length > 0) {
+		for (var customer in burgerTown.queue) {
 
-		// Looks at whether the order is fulfilled, makes them angry if it's not.
-		if (burgerTown.queue[customer].order.completion == 100) {
-			console.log("Order completed")
-			burgerTown.netEarning += burgerTown.queue[customer].order.orderTotal;
-			burgerTown.queue.splice(customer, 1)
-			burgerTown.customerServed += 1
-		} else {
-			burgerTown.queue[customer].anger += 5;
-		}
+			// Looks at whether the order is fulfilled, makes them angry if it's not.
+			if (burgerTown.queue[customer].order.completion == 100) {
+				console.log("Order completed")
+				burgerTown.netEarning += burgerTown.queue[customer].order.orderTotal;
+				burgerTown.queue.splice(customer, 1)
+				burgerTown.customerServed += 1
+			} else {
+				burgerTown.queue[customer].anger += 5;
+			}
 
-		// Customers walk out without paying if they're too angry
-		if (burgerTown.queue[customer].anger >= 80) {
-			burgerTown.queue.splice(customer, 1)
-			burgerTown.customerWalkouts += 1
-			console.log("The customer got too irate and walked out.")
+			// Customers walk out without paying if they're too angry
+			if (burgerTown.queue[customer].anger >= 80) {
+				burgerTown.queue.splice(customer, 1)
+				burgerTown.customerWalkouts += 1
+				console.log("The customer got too irate and walked out.")
+			}
 		}
 	}
 
